53a620bd9b3a2d692bd469c823124b4da001f266
Thibault Charbonnier <thibaultcha@me.com>
2018-09-28 12:31:17 -0700

    fix: make 'kong' owner of 'public' schema

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -7,4 +7,8 @@ psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE DATABASE kong_tests;
     GRANT ALL PRIVILEGES ON DATABASE kong TO kong;
     GRANT ALL PRIVILEGES ON DATABASE kong_tests TO kong;
+    \c kong;
+    ALTER SCHEMA public OWNER TO kong;
+    \c kong_tests;
+    ALTER SCHEMA public OWNER TO kong;
 EOSQL
