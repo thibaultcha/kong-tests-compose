cf2aecea58eb17602f6489fe348c4bab3e9c5f8c
Thibault Charbonnier <thibaultcha@me.com>
2020-03-10 13:44:24 -0700

    Set auth method to 'trust' and specify postgres password

--- postgres/init-user-db.sh
+++ postgres/init-user-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+PG_PASSWORD=postgres psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
     CREATE user kong;
     CREATE DATABASE kong;
     CREATE DATABASE kong_tests;
